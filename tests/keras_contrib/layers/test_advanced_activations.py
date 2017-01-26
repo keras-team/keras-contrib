1be552eb6de093f3f7c394b7f9edd5fc3e01d1dd
Fariz Rahman <farizrahman4u@gmail.com>
2017-01-26 11:37:40 +0530

    Import on top of file

--- tests/keras_contrib/layers/test_advanced_activations.py
+++ tests/keras_contrib/layers/test_advanced_activations.py
@@ -1,19 +1,17 @@
 import pytest
 from keras.utils.test_utils import layer_test, keras_test
-from keras_contrib import layers
+from keras_contrib.layers import advanced_activations
 
 
 @keras_test
 def test_pelu():
-    from keras_contrib.layers.advanced_activations import PELU
-    layer_test(PELU, kwargs={},
+    layer_test(advanced_activations.PELU, kwargs={},
                input_shape=(2, 3, 4))
 
 
 @keras_test
 def test_pelu_share():
-    from keras_contrib.layers.advanced_activations import PELU
-    layer_test(PELU, kwargs={'shared_axes': 1},
+    layer_test(advanced_activations.PELU, kwargs={'shared_axes': 1},
                input_shape=(2, 3, 4))
 
 
