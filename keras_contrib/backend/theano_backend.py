1bc0c8bff0bf45c3d2b78ed48d9dd8a8e5b72215
abhai <abhai.dilip@gmail.com>
2017-03-12 02:33:36 +0530

    Backport theano backend

--- keras_contrib/backend/theano_backend.py
+++ keras_contrib/backend/theano_backend.py
@@ -34,7 +34,7 @@ py_all = all
 
 def deconv3d(x, kernel, output_shape, strides=(1, 1, 1),
              border_mode='valid',
-             dim_ordering='default',
+             data_format='default',
              image_shape=None, filter_shape=None):
     '''3D deconvolution (transposed convolution).
 
@@ -43,24 +43,24 @@ def deconv3d(x, kernel, output_shape, strides=(1, 1, 1),
         output_shape: desired dimensions of output.
         strides: strides tuple.
         border_mode: string, "same" or "valid".
-        dim_ordering: "tf" or "th".
+        data_format: "channels_last" or "channels_first".
             Whether to use Theano or TensorFlow dimension ordering
         in inputs/kernels/ouputs.
     '''
     flip_filters = False
-    if dim_ordering == 'default':
-        dim_ordering = image_dim_ordering()
-    if dim_ordering not in {'th', 'tf'}:
-        raise ValueError('Unknown dim_ordering ' + str(dim_ordering))
+    if data_format == 'default':
+        data_format = image_dim_ordering()
+    if data_format not in {'channels_last', 'channels_first'}:
+        raise ValueError('Unknown image data format ' + str(data_format))
 
-    if dim_ordering == 'tf':
+    if daa_format == 'channels_last':
         output_shape = (output_shape[0], output_shape[4], output_shape[1],
                         output_shape[2], output_shape[3])
 
-    x = _preprocess_conv3d_input(x, dim_ordering)
-    kernel = _preprocess_conv3d_kernel(kernel, dim_ordering)
+    x = _preprocess_conv3d_input(x, data_format)
+    kernel = _preprocess_conv3d_kernel(kernel, data_format)
     kernel = kernel.dimshuffle((1, 0, 2, 3, 4))
-    th_border_mode = _preprocess_border_mode(border_mode)
+    th_padding = _preprocess_padding(padding)
 
     if hasattr(kernel, '_keras_shape'):
         kernel_shape = kernel._keras_shape
@@ -68,22 +68,22 @@ def deconv3d(x, kernel, output_shape, strides=(1, 1, 1),
         # Will only work if `kernel` is a shared variable.
         kernel_shape = kernel.eval().shape
 
-    filter_shape = _preprocess_conv3d_filter_shape(dim_ordering, filter_shape)
+    filter_shape = _preprocess_conv3d_filter_shape(data_format, filter_shape)
     filter_shape = tuple(filter_shape[i] for i in (1, 0, 2, 3, 4))
 
     conv_out = T.nnet.abstract_conv.conv3d_grad_wrt_inputs(
         x, kernel, output_shape,
         filter_shape=filter_shape,
-        border_mode=th_border_mode,
+        border_mode=th_padding,
         subsample=strides,
         filter_flip=not flip_filters)
 
-    conv_out = _postprocess_conv3d_output(conv_out, x, border_mode,
-                                          kernel_shape, strides, dim_ordering)
+    conv_out = _postprocess_conv3d_output(conv_out, x, padding,
+                                          kernel_shape, strides, data_format)
     return conv_out
 
 
-def extract_image_patches(X, ksizes, strides, border_mode="valid", dim_ordering="th"):
+def extract_image_patches(X, ksizes, strides, padding="valid", data_format="channels_first"):
     '''
     Extract the patches from an image
     Parameters
@@ -91,8 +91,8 @@ def extract_image_patches(X, ksizes, strides, border_mode="valid", dim_ordering=
     X : The input image
     ksizes : 2-d tuple with the kernel size
     strides : 2-d tuple with the strides size
-    border_mode : 'same' or 'valid'
-    dim_ordering : 'tf' or 'th'
+    padding : 'same' or 'valid'
+    data_format : 'channels_last' or 'channels_first'
     Returns
     -------
     The (k_w,k_h) patches extracted
@@ -100,9 +100,9 @@ def extract_image_patches(X, ksizes, strides, border_mode="valid", dim_ordering=
     TH ==> (batch_size,w,h,c,k_w,k_h)
     '''
     patch_size = ksizes[1]
-    if border_mode == "same":
-        border_mode = "ignore_borders"
-    if dim_ordering == "tf":
+    if padding == "same":
+        padding = "ignore_borders"
+    if data_format == "channels_last":
         X = KTH.permute_dimensions(X, [0, 3, 1, 2])
     # Thanks to https://github.com/awentzonline for the help!
     batch, c, w, h = KTH.shape(X)
@@ -110,13 +110,13 @@ def extract_image_patches(X, ksizes, strides, border_mode="valid", dim_ordering=
     num_rows = 1 + (xs[-2] - patch_size) // strides[1]
     num_cols = 1 + (xs[-1] - patch_size) // strides[1]
     num_channels = xs[-3]
-    patches = images2neibs(X, ksizes, strides, border_mode)
+    patches = images2neibs(X, ksizes, strides, padding)
     # Theano is sorting by channel
     patches = KTH.reshape(patches, (batch, num_channels, KTH.shape(patches)[0] // num_channels, patch_size, patch_size))
     patches = KTH.permute_dimensions(patches, (0, 2, 1, 3, 4))
     # arrange in a 2d-grid (rows, cols, channels, px, py)
     patches = KTH.reshape(patches, (batch, num_rows, num_cols, num_channels, patch_size, patch_size))
-    if dim_ordering == "tf":
+    if data_format == "channels_last":
         patches = KTH.permute_dimensions(patches, [0, 1, 2, 4, 5, 3])
     return patches
 
@@ -124,7 +124,7 @@ def extract_image_patches(X, ksizes, strides, border_mode="valid", dim_ordering=
 def depth_to_space(input, scale):
     ''' Uses phase shift algorithm to convert channels/depth for spatial resolution '''
 
-    input = _preprocess_conv2d_input(input, image_dim_ordering())
+    input = _preprocess_conv2d_input(input, image_data_format())
 
     b, k, row, col = input.shape
     output_shape = (b, k // (scale ** 2), row * scale, col * scale)
@@ -135,7 +135,7 @@ def depth_to_space(input, scale):
     for y, x in itertools.product(range(scale), repeat=2):
         out = T.inc_subtensor(out[:, :, y::r, x::r], input[:, r * y + x:: r * r, :, :])
 
-    out = _postprocess_conv2d_output(out, input, None, None, None, image_dim_ordering())
+    out = _postprocess_conv2d_output(out, input, None, None, None, image_data_format())
     return out
 
 
