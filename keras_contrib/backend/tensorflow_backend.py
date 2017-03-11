409ce655eb4ddb0dae032fd7141cd911ec8e715a
abhai <abhai.dilip@gmail.com>
2017-03-12 02:16:50 +0530

    Backport tensorflow backend

--- keras_contrib/backend/tensorflow_backend.py
+++ keras_contrib/backend/tensorflow_backend.py
@@ -14,7 +14,6 @@ import warnings
 from keras.backend.common import floatx, _EPSILON, image_data_format, reset_uids
 from keras.backend.tensorflow_backend import _preprocess_conv3d_input
 from keras.backend.tensorflow_backend import _preprocess_conv3d_kernel
-from keras.backend.tensorflow_backend import _preprocess_border_mode
 from keras.backend.tensorflow_backend import _postprocess_conv3d_output
 from keras.backend.tensorflow_backend import _preprocess_padding
 from keras.backend.tensorflow_backend import _preprocess_conv2d_input
@@ -23,8 +22,8 @@ from keras.backend.tensorflow_backend import _postprocess_conv2d_output
 py_all = all
 
 
-def _preprocess_deconv_output_shape(x, shape, dim_ordering):
-    if dim_ordering == 'th':
+def _preprocess_deconv_output_shape(x, shape, data_format):
+    if data_format == 'channels_first':
         shape = (shape[0],) + tuple(shape[2:]) + (shape[1],)
 
     if shape[0] is None:
@@ -34,8 +33,8 @@ def _preprocess_deconv_output_shape(x, shape, dim_ordering):
 
 
 def deconv3d(x, kernel, output_shape, strides=(1, 1, 1),
-             border_mode='valid',
-             dim_ordering='default',
+             padding='valid',
+             data_format='default',
              image_shape=None, filter_shape=None):
     '''3D deconvolution (i.e. transposed convolution).
 
@@ -44,8 +43,8 @@ def deconv3d(x, kernel, output_shape, strides=(1, 1, 1),
         kernel: kernel tensor.
         output_shape: 1D int tensor for the output shape.
         strides: strides tuple.
-        border_mode: string, "same" or "valid".
-        dim_ordering: "tf" or "th".
+        padding: string, "same" or "valid".
+        data_format: "channels_last" or "channels_first".
             Whether to use Theano or TensorFlow dimension ordering
             for inputs/kernels/ouputs.
 
@@ -53,28 +52,29 @@ def deconv3d(x, kernel, output_shape, strides=(1, 1, 1),
         A tensor, result of transposed 3D convolution.
 
     # Raises
-        ValueError: if `dim_ordering` is neither `tf` or `th`.
+        ValueError: if `image_data_format` is neither 
+                    `channels_last` or `channels_first`.
     '''
-    if dim_ordering == 'default':
-        dim_ordering = image_dim_ordering()
-    if dim_ordering not in {'th', 'tf'}:
-        raise ValueError('Unknown dim_ordering ' + str(dim_ordering))
+    if data_format == 'default':
+        data_format = image_data_format()
+    if data_format not in {'channels_last', 'channels_first'}:
+        raise ValueError('Unknown image data format ' + str(data_format))
 
-    x = _preprocess_conv3d_input(x, dim_ordering)
+    x = _preprocess_conv3d_input(x, data_format)
     output_shape = _preprocess_deconv_output_shape(x, output_shape,
-                                                   dim_ordering)
-    kernel = _preprocess_conv3d_kernel(kernel, dim_ordering)
+                                                   data_format)
+    kernel = _preprocess_conv3d_kernel(kernel, data_format)
     kernel = tf.transpose(kernel, (0, 1, 2, 4, 3))
-    padding = _preprocess_border_mode(border_mode)
+    padding = _preprocess_padding(padding)
     strides = (1,) + strides + (1,)
 
     x = tf.nn.conv3d_transpose(x, kernel, output_shape, strides,
                                padding=padding)
-    return _postprocess_conv3d_output(x, dim_ordering)
+    return _postprocess_conv3d_output(x, data_format)
 
 
-def extract_image_patches(x, ksizes, ssizes, border_mode="same",
-                          dim_ordering="tf"):
+def extract_image_patches(x, ksizes, ssizes, padding="same",
+                          data_format="channels_last"):
     '''
     Extract the patches from an image
     # Parameters
@@ -82,8 +82,8 @@ def extract_image_patches(x, ksizes, ssizes, border_mode="same",
         x : The input image
         ksizes : 2-d tuple with the kernel size
         ssizes : 2-d tuple with the strides size
-        border_mode : 'same' or 'valid'
-        dim_ordering : 'tf' or 'th'
+        padding : 'same' or 'valid'
+        data_format : 'channels_last' or 'channels_first'
 
     # Returns
         The (k_w,k_h) patches extracted
@@ -92,8 +92,8 @@ def extract_image_patches(x, ksizes, ssizes, border_mode="same",
     '''
     kernel = [1, ksizes[0], ksizes[1], 1]
     strides = [1, ssizes[0], ssizes[1], 1]
-    padding = _preprocess_border_mode(border_mode)
-    if dim_ordering == "th":
+    padding = _preprocess_padding(padding)
+    if data_format == "channels_first":
         x = KTF.permute_dimensions(x, (0, 2, 3, 1))
     bs_i, w_i, h_i, ch_i = KTF.int_shape(x)
     patches = tf.extract_image_patches(x, kernel, strides, [1, 1, 1, 1],
@@ -103,7 +103,7 @@ def extract_image_patches(x, ksizes, ssizes, border_mode="same",
     patches = tf.reshape(patches, [bs, w, h, -1, ch_i])
     patches = tf.reshape(tf.transpose(patches, [0, 1, 2, 4, 3]),
                          [bs, w, h, ch_i, ksizes[0], ksizes[1]])
-    if dim_ordering == "tf":
+    if data_format == "channels_last":
         patches = KTF.permute_dimensions(patches, [0, 1, 2, 4, 5, 3])
     return patches
 
@@ -111,9 +111,9 @@ def extract_image_patches(x, ksizes, ssizes, border_mode="same",
 def depth_to_space(input, scale):
     ''' Uses phase shift algorithm to convert channels/depth for spatial resolution '''
 
-    input = _preprocess_conv2d_input(input, image_dim_ordering())
+    input = _preprocess_conv2d_input(input, image_data_format())
     out = tf.depth_to_space(input, scale)
-    out = _postprocess_conv2d_output(out, image_dim_ordering())
+    out = _postprocess_conv2d_output(out, image_data_format())
     return out
 
 
