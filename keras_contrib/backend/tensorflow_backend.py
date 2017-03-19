52ceb7afaf83774fd899a8e840ae38f6306d8944
farizrahman4u <farizrahman4u@gmail.com>
2017-03-19 08:01:00 +0530

    fix tf backend too

--- keras_contrib/backend/tensorflow_backend.py
+++ keras_contrib/backend/tensorflow_backend.py
@@ -155,12 +155,14 @@ def extract_image_patches(x, ksizes, ssizes, padding="same",
     return patches
 
 
-def depth_to_space(input, scale):
+def depth_to_space(input, scale, data_format=None):
     ''' Uses phase shift algorithm to convert channels/depth for spatial resolution '''
-
-    input = _preprocess_conv2d_input(input, image_data_format())
+    if data_format is None:
+        data_format = image_data_format()
+    data_format = data_format.lower()
+    input = _preprocess_conv2d_input(input, data_format)
     out = tf.depth_to_space(input, scale)
-    out = _postprocess_conv2d_output(out, image_data_format())
+    out = _postprocess_conv2d_output(out, data_format)
     return out
 
 
