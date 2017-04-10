a66f3141e9bf59252d17a7c60909e4b85644157d
Sourav Singh <souravsingh@users.noreply.github.com>
2017-04-10 21:04:11 +0530

    Create lsun.py

---
+++ keras_contrib/datasets/lsun.py
@@ -0,0 +1,19 @@
+from __future__ import print_function, division
+import argparse
+import json
+from os.path import join
+
+import subprocess
+import urllib2
+
+def download(out_dir, category, set_name, tag):
+    url = 'http://lsun.cs.princeton.edu/htbin/download.cgi?tag={tag}' \
+          '&category={category}&set={set_name}'.format(**locals())
+    if set_name == 'test':
+        out_name = 'test_lmdb.zip'
+    else:
+        out_name = '{category}_{set_name}_lmdb.zip'.format(**locals())
+    out_path = join(out_dir, out_name)
+    cmd = ['curl', url, '-o', out_path]
+    print('Downloading', category, set_name, 'set')
+    subprocess.call(cmd)
