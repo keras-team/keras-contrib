58a0d51ae67a44835f755f343677ad291235274f
Michael Oliver <michael.d.oliver@gmail.com>
2017-06-14 15:04:26 -0700

    Fix PEP8 error

--- keras_contrib/backend/__init__.py
+++ keras_contrib/backend/__init__.py
@@ -13,4 +13,4 @@ if K.backend() == 'theano':
 elif K.backend() == 'tensorflow':
     from .tensorflow_backend import *
 elif K.backend() == 'cntk':
-	from .cntk_backend import *
+    from .cntk_backend import *
