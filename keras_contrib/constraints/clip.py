6936cf0de733bceac279f02c776cf8c22ed942e2
Michael Oliver <michael.d.oliver@gmail.com>
2017-06-15 17:02:49 -0700

    move constraints and initializers

---
+++ keras_contrib/constraints/clip.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from . import backend as K
+from keras.constraints import *
+
+
+class Clip(Constraint):
+    """Clips weights to [-c, c].
+
+    # Arguments
+        c: Clipping parameter.
+    """
+
+    def __init__(self, c=0.01):
+        self.c = c
+
+    def __call__(self, p):
+        return K.clip(p, -self.c, self.c)
+
+    def get_config(self):
+        return {'name': self.__class__.__name__,
+                'c': self.c}
+
+# Aliases.
+
+clip = Clip
