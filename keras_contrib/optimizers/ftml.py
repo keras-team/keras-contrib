4c046972a540edd547d3593caaa6234bddf60176
Michael Oliver <michael.d.oliver@gmail.com>
2017-08-16 20:40:17 -0700

    fix import

--- keras_contrib/optimizers/ftml.py
+++ keras_contrib/optimizers/ftml.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from keras.optimizers import Optimizer
 from .. import backend as K
 from keras.utils.generic_utils import get_custom_objects
+from keras.legacy import interfaces
 
 
 class FTML(Optimizer):
