fix normalization tests

keras_contrib/layers/normalization.py
@@ -219,7 +219,7 @@ class BatchRenormalization(Layer):
         self.initial_weights = weights
         self.r_max_value = r_max_value
         self.d_max_value = d_max_value
-        self.t_delta = K.variable(np.array(t_delta))
+        self.t_delta = t_delta
         self.beta_initializer = initializers.get(beta_initializer)
         self.gamma_initializer = initializers.get(gamma_initializer)
         self.moving_mean_initializer = initializers.get(moving_mean_initializer)
@@ -272,6 +272,8 @@ class BatchRenormalization(Layer):
 
         self.t = K.variable(np.zeros((1,)), name='{}_t'.format(self.name))
 
+        self.t_delta_tensor = K.variable(np.array([self.t_delta]))
+
         if self.initial_weights is not None:
             self.set_weights(self.initial_weights)
             del self.initial_weights
@@ -323,7 +325,7 @@ class BatchRenormalization(Layer):
 
         self.add_update([K.update(self.r_max, r_val),
                          K.update(self.d_max, d_val),
-                         K.update_add(self.t, self.t_delta)], x)
+                         K.update_add(self.t, self.t_delta_tensor)], inputs)
 
         if training in {0, False}:
             return x_normed
@@ -358,13 +360,15 @@ class BatchRenormalization(Layer):
     def get_config(self):
         config = {'epsilon': self.epsilon,
                   'axis': self.axis,
+                  'center': self.center,
+                  'scale': self.scale,
+                  'momentum': self.momentum,
                   'gamma_regularizer': initializers.serialize(self.gamma_regularizer),
                   'beta_regularizer': initializers.serialize(self.beta_regularizer),
                   'moving_mean_initializer': initializers.serialize(self.moving_mean_initializer),
                   'moving_variance_initializer': initializers.serialize(self.moving_variance_initializer),
                   'beta_constraint': constraints.serialize(self.beta_constraint),
                   'gamma_constraint': constraints.serialize(self.gamma_constraint),
-                  'momentum': self.momentum,
                   'r_max_value': self.r_max_value,
                   'd_max_value': self.d_max_value,
                   't_delta': self.t_delta}