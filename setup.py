48c840bda0f295fba6788fa35460f9118cfbb3be
Andrew Hundt <ATHundt@gmail.com>
2017-09-25 13:27:02 -0400

    setup.py update to match keras

--- setup.py
+++ setup.py
@@ -3,11 +3,32 @@ from setuptools import find_packages
 
 
 setup(name='keras_contrib',
-      version='1.2.1',
-      description='Keras community contributions',
+      version='2.0.8',
+      description='Keras Deep Learning for Python, Community Contributions',
       author='Fariz Rahman',
       author_email='farizrahman4u@gmail.com',
       url='https://github.com/farizrahman4u/keras-contrib',
       license='MIT',
       install_requires=['keras'],
+      extras_require={
+          'h5py': ['h5py'],
+          'visualize': ['pydot>=1.2.0'],
+          'tests': ['pytest',
+                    'pytest-pep8',
+                    'pytest-xdist',
+                    'pytest-cov'],
+      },
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
       packages=find_packages())
