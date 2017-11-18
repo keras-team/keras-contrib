2f95542184c74f734c93c933185f89b4ff724c30
Valentin <valentin_deyringer@web.de>
2017-11-18 16:54:23 +0100

    adds tests for swish activation

--- tests/keras_contrib/layers/test_advanced_activations.py
+++ tests/keras_contrib/layers/test_advanced_activations.py
@@ -26,5 +26,17 @@ def test_srelu_share():
     layer_test(advanced_activations.SReLU, kwargs={'shared_axes': 1},
                input_shape=(2, 3, 4))
 
+@keras_test
+def test_swish_constant():
+    layer_test(advanced_activations.Swish, kwargs={'beta': 1.0, 'trainable': False},
+               input_shape=(2, 3, 4))
+
+
+@keras_test
+def test_swish_trainable():
+    layer_test(advanced_activations.Swish, kwargs={'beta': 1.0, 'trainable': True},
+               input_shape=(2, 3, 4))
+
+
 if __name__ == '__main__':
     pytest.main([__file__])
