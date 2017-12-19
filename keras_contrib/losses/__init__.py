80b731b39b0fc00e1690324c984de1c5285e56c2
Andrew Hundt <athundt@gmail.com>
2017-12-19 13:05:14 -0500

    losses and metrics __init__.py update

--- keras_contrib/losses/__init__.py
+++ keras_contrib/losses/__init__.py
@@ -1,1 +1,9 @@
+from __future__ import absolute_import
+from . import segmentation_losses
+from . import dssim
+
+# Globally-importable losses
+from .segmentation_losses import binary_crossentropy
+from .segmentation_losses import binary_crossentropy_excluding_first_label
+
 from .dssim import DSSIMObjective
---
+++ keras_contrib/metrics/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from . import segmentation_metrics
+
+# Globally-importable metrics
+from .segmentation_metrics import pixel_accuracy
+from .segmentation_metrics import mean_accuracy
+from .segmentation_metrics import mean_intersection_over_union
