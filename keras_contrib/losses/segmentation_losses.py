ee8a7ce4e6d7c063128bd8ec33fe396d291931d8
Andrew Hundt <athundt@gmail.com>
2017-12-19 15:48:06 -0500

    segmentation_losses.py compatible with from_logits

--- keras_contrib/losses/segmentation_losses.py
+++ keras_contrib/losses/segmentation_losses.py
@@ -7,13 +7,14 @@ import tensorflow as tf
 import sys
 
 
-def binary_crossentropy(y_true, y_pred):
+def binary_crossentropy(y_true, y_pred, from_logits=False):
     pred_shape = K.int_shape(y_pred)
     true_shape = K.int_shape(y_true)
     y_pred_reshaped = K.reshape(y_pred, (-1, pred_shape[-1]))
     y_true_reshaped = K.reshape(y_true, (-1, true_shape[-1]))
-    print('pred_shape: ' + str(pred_shape), 'pred_shape: ' + str(pred_shape), )
-    result = K.binary_crossentropy(y_pred_reshaped, y_true_reshaped)
+
+    result = K.binary_crossentropy(y_pred_reshaped, y_true_reshaped,
+                                   from_logits=from_logits)
     result = K.mean(result, axis=-1)
 
     if len(true_shape) >= 3:
