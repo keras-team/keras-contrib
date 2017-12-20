33901e9efc6626ed6fa3b50eb9b272620ec4a6ed
Andrew Hundt <ATHundt@gmail.com>
2017-12-20 10:01:18 -0500

    jaccard_loss.py added, fix jaccard.py documentation pep8

--- keras_contrib/losses/jaccard.py
+++ keras_contrib/losses/jaccard.py
@@ -2,20 +2,30 @@ from keras import backend as K
 
 
 def jaccard_distance(y_true, y_pred, smooth=100):
-    """
-    Jaccard distance is a intersection-over-union loss which is not the defacto
-    standard for image segementation. It's a usefull loss when you have
-    unbalanced numbers of pixels within an image because it gives all classes
-    equal weight.
+    """Jaccard distance is an intersection-over-union loss for semantic segmentation
 
-    For example, you are trying to predict if each pixel is cat, dog, or background.
+    This loss is useful when you have unbalanced numbers of pixels within an image
+    because it gives all classes equal weight. However, it is not the defacto
+    standard for image segmentation.
+
+    For example, assume you are trying to predict if each pixel is cat, dog, or background.
     You have 80% background pixels, 10% dog, and 10% cat. If the model predicts 100% background
     should it be be 80% right (as with categorical cross entropy) or 30% (with this loss)?
 
     The loss has been modified to have a smooth gradient as it converges on zero.
+    This has been shifted so it converges on 0 and is smoothed to avoid exploding
+    or disappearing gradient.
 
-    Refs:
-    - Csurka, Gabriela & Larlus, Diane & Perronnin, Florent. (2013). What is a good evaluation measure for semantic segmentation?. IEEE Trans. Pattern Anal. Mach. Intell.. 26. . 10.5244/C.27.32. 
+    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+
+    # References
+
+    Csurka, Gabriela & Larlus, Diane & Perronnin, Florent. (2013).
+    What is a good evaluation measure for semantic segmentation?.
+    IEEE Trans. Pattern Anal. Mach. Intell.. 26. . 10.5244/C.27.32.
+
+    https://en.wikipedia.org/wiki/Jaccard_index
 
     """
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
