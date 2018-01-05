db87a2e729dfc305ec56a22c2c2b77f824bf1336
jeffpan <2359501@qq.com>
2018-01-05 10:21:49 +0800

    add logsumexp in tensorflow_backend.py
    logsumexp is needed in crf.py

--- keras_contrib/backend/tensorflow_backend.py
+++ keras_contrib/backend/tensorflow_backend.py
@@ -187,3 +187,23 @@ def clip(x, min_value, max_value):
     max_value = _to_tensor(max_value, x.dtype.base_dtype)
     max_value = tf.maximum(min_value, max_value)
     return tf.clip_by_value(x, min_value, max_value)
+
+def logsumexp(x, axis=None, keepdims=False):
+    """Computes log(sum(exp(elements across dimensions of a tensor))).
+
+    This function is more numerically stable than log(sum(exp(x))).
+    It avoids overflows caused by taking the exp of large inputs and
+    underflows caused by taking the log of small inputs.
+
+    # Arguments
+        x: A tensor or variable.
+        axis: An integer, the axis to reduce over.
+        keepdims: A boolean, whether to keep the dimensions or not.
+            If `keepdims` is `False`, the rank of the tensor is reduced
+            by 1. If `keepdims` is `True`, the reduced dimension is
+            retained with length 1.
+
+    # Returns
+        The reduced tensor.
+    """
+    return tf.reduce_logsumexp(x, axis, keepdims)
