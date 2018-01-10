6630ee62b5b10acdbd3b5bcf4c502e6571c8a7d6
Andrew Hundt <ATHundt@gmail.com>
2018-01-10 09:04:07 -0500

    densenet.py include_top updated to require_flatten in upstream keras

--- keras_contrib/applications/densenet.py
+++ keras_contrib/applications/densenet.py
@@ -202,7 +202,7 @@ def DenseNet(input_shape=None,
                                       default_size=32,
                                       min_size=8,
                                       data_format=K.image_data_format(),
-                                      include_top=include_top)
+                                      require_flatten=include_top)
 
     if input_tensor is None:
         img_input = Input(shape=input_shape)
