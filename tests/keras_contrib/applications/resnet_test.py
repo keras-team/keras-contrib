0af5bd34c862528a95b1a4487b3b6a0edfdc2c31
Andrew Hundt <ATHundt@gmail.com>
2018-02-03 20:38:16 -0500

    resnet_test.py simplify unit test

--- tests/keras_contrib/applications/resnet_test.py
+++ tests/keras_contrib/applications/resnet_test.py
@@ -13,20 +13,15 @@ from keras_contrib.applications.resnet import ResNet152
 DIM_ORDERING = {'channels_first', 'channels_last'}
 
 
-def _test_model_compile(model_fn, test_dims, time_distributed=False, predict=False):
-    if time_distributed:
-        time, width, height, channels, classes = test_dims
-    else:
-        width, height, channels, classes = test_dims
+def _test_model_compile(model_fn, test_dims, predict=False):
+    width, height, channels, classes = test_dims
     for ordering in DIM_ORDERING:
         K.set_image_data_format(ordering)
         if ordering == 'channels_first':
             input_dim = (channels, width, height)
         if ordering == 'channels_last':
             input_dim = (width, height, channels)
-        if time_distributed:
-            input_dim = (time,) + input_dim
-        model = model_fn(input_dim, classes, time_distributed=time_distributed)
+        model = model_fn(input_dim, classes)
         model.compile(loss="categorical_crossentropy", optimizer="sgd")
         # conditionally predict to reduce test runtime
         if predict:
@@ -38,25 +33,12 @@ def _test_model_compile(model_fn, test_dims, time_distributed=False, predict=Fal
 def test_resnet():
     # [(width, height, channels, classes), ...]
     test_dims = [(200, 224, 3, 3)]
-    time_distributed = False
     for dims in test_dims:
-        _test_model_compile(ResNet18, dims, time_distributed=time_distributed, predict=True)
-        _test_model_compile(ResNet34, dims, time_distributed=time_distributed)
-        _test_model_compile(ResNet50, dims, time_distributed=time_distributed)
-        _test_model_compile(ResNet101, dims, time_distributed=time_distributed)
-        _test_model_compile(ResNet152, dims, time_distributed=time_distributed)
-
-
-def test_resnet_time_distributed():
-    # [(time, width, height, channels, classes), ...]
-    test_dims = [(2, 224, 224, 3, 3)]
-    time_distributed = True
-    for dims in test_dims:
-        _test_model_compile(ResNet18, dims, time_distributed=time_distributed, predict=True)
-        _test_model_compile(ResNet34, dims, time_distributed=time_distributed)
-        _test_model_compile(ResNet50, dims, time_distributed=time_distributed)
-        _test_model_compile(ResNet101, dims, time_distributed=time_distributed)
-        _test_model_compile(ResNet152, dims, time_distributed=time_distributed)
+        _test_model_compile(ResNet18, dims, predict=True)
+        _test_model_compile(ResNet34, dims)
+        _test_model_compile(ResNet50, dims)
+        _test_model_compile(ResNet101, dims)
+        _test_model_compile(ResNet152, dims)
 
 if __name__ == '__main__':
     pytest.main([__file__])
