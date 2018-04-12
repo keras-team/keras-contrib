1fae9627eb70348222819bdcc880fff268026514
TanguyUrvoy <tanguy.urvoy@orange.com>
2018-04-12 18:33:08 +0200

    python 2/3 compatibility

--- keras_contrib/tests/preprocessing/sequence_test.py
+++ keras_contrib/tests/preprocessing/sequence_test.py
@@ -1,7 +1,11 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
 import numpy as np
 from numpy.testing import assert_allclose
-import pytest
 from keras_contrib.preprocessing.sequence import TimeseriesGenerator
+import pytest
 
 
 def test_TimeseriesGenerator():
