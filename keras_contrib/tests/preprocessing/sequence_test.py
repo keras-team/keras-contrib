fbef33ead5b9d66fa14a78b11fba81353673e574
TanguyUrvoy <tanguy.urvoy@orange.com>
2018-04-13 10:37:06 +0200

    Python 2/3 compatibility

--- keras_contrib/tests/preprocessing/sequence_test.py
+++ keras_contrib/tests/preprocessing/sequence_test.py
@@ -10,7 +10,7 @@ import pytest
 
 def test_TimeseriesGenerator():
 
-    print "** test 0 (float types)"
+    print("** test 0 (float types)")
 
     data = np.array([[i] for i in range(50)], dtype=np.float)
     targets = np.array([[float(i)] for i in range(50)])
@@ -24,7 +24,7 @@ def test_TimeseriesGenerator():
                                     [[1], [3], [5], [7], [9]]]))
     assert np.allclose(y, np.array([[10], [11]]))
 
-    print "** test 1 (auto types)"
+    print("** test 1 (auto types)")
 
     data = np.array([[i] for i in range(50)], dtype=np.float)
     targets = np.array([[i] for i in range(50)], dtype=np.float)
@@ -44,7 +44,7 @@ def test_TimeseriesGenerator():
                                        [[39], [41], [43], [45], [47]]]))
     assert np.array_equal(y, np.array([[48], [49]]))
 
-    print "** test 2 (batch_size=4)"
+    print("** test 2 (batch_size=4)")
     data_gen = TimeseriesGenerator(data, targets, length=10, batch_size=4)
     assert len(data_gen) == 10
     x, y = data_gen[0]
@@ -57,41 +57,41 @@ def test_TimeseriesGenerator():
     x, y = data_gen[0]
     assert np.array_equal(x[1, 0], np.array([10]))
 
-    print "** test 3 (when sampling_rate is not a multiple of length)"
+    print("** test 3 (when sampling_rate is not a multiple of length)")
     data_gen = TimeseriesGenerator(
         data, targets, length=10, sampling_rate=3, batch_size=2)
 
     assert len(data_gen) == 10
 
-    print "** test 4 (stateful)"
+    print("** test 4 (stateful)")
     data_gen = TimeseriesGenerator(
         data, targets, length=10, sampling_rate=2, batch_size=12, stateful=True)
 
     assert data_gen.stride == 2
     assert data_gen.batch_size == 10
 
-    print "** test 5 (text sequences seq2one)"
-    txt = bytearray("Keras is simple.")
+    print("** test 5 (text sequences seq2one)")
+    txt = bytearray("Keras is simple.", 'utf-8')
     data_gen = TimeseriesGenerator(txt, txt, length=10, batch_size=1)
 
     # for i in range(len(data_gen)):
-    #    print data_gen[i][0].tostring(), " -> '%s'" % data_gen[i][1].tostring()
+    #    print(data_gen[i][0].tostring(), "->'%s'" % data_gen[i][1].tostring())
 
     assert data_gen[-1][0].shape == (1, 10) and data_gen[-1][1].shape == (1,)
     assert data_gen[-1][0].tostring() == u" is simple"
     assert data_gen[-1][1].tostring() == u"."
 
-    print "** test 6 (text sequences seq2seq)"
+    print("** test 6 (text sequences seq2seq)")
     data_gen = TimeseriesGenerator(txt, txt, length=10, target_seq=True)
 
     assert data_gen[-1][0].shape == (1,
                                      10) and data_gen[-1][1].shape == (1, 10, 1)
     # for i in range(len(data_gen)):
-    #    print data_gen[i][0].tostring(), " -> '%s'" % data_gen[i][1].tostring()
+    #    print(data_gen[i][0].tostring(), "->'%s'" % data_gen[i][1].tostring())
 
     assert data_gen[0][1].tostring() == u"eras is si"
 
-    print "** previous tests"
+    print("** previous tests (modified for new length semantic)")
 
     data_gen = TimeseriesGenerator(data, targets,
                                    length=5, sampling_rate=2, reverse=True,
