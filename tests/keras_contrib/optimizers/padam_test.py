49171e37de074b756cf2114dc52e69cb4e38b3f3
MFreidank <freidankm@yahoo.de>
2018-09-05 10:15:23 +0200

    added tests for padam optimizer
    setup follows adam tests in keras

---
+++ tests/keras_contrib/optimizers/padam_test.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+from keras_contrib.tests import optimizers
+from keras_contrib.optimizers import Padam
+
+optimizers._test_optimizer(Padam())
+optimizers._test_optimizer(Padam(decay=1e-3))
