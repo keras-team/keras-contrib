62bc8b3aaa6f40bdef2632b3f4133af417681f2a
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2018-12-26 15:44:43 +0100

    Fixed docstring style.

--- keras_contrib/losses/jaccard.py
+++ keras_contrib/losses/jaccard.py
@@ -2,8 +2,9 @@ from keras import backend as K
 
 
 def jaccard_distance(y_true, y_pred, smooth=100):
-    """Jaccard distance for semantic segmentation,
-    also known as the intersection-over-union loss.
+    """Jaccard distance for semantic segmentation.
+
+    Also known as the intersection-over-union loss.
 
     This loss is useful when you have unbalanced numbers of pixels within an image
     because it gives all classes equal weight. However, it is not the defacto
