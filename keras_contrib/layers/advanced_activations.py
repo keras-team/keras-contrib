ba0b36e0f501841a9d95b45b773ea8572070fa18
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2018-12-26 16:20:14 +0100

    Made lines shorter in advanced_activations.py (#369)

--- keras_contrib/layers/advanced_activations.py
+++ keras_contrib/layers/advanced_activations.py
@@ -32,7 +32,8 @@ class PELU(Layer):
             so that each filter only has one set of parameters,
             set `shared_axes=[1, 2]`.
     # References
-        - [PARAMETRIC EXPONENTIAL LINEAR UNIT FOR DEEP CONVOLUTIONAL NEURAL NETWORKS](https://arxiv.org/abs/1605.09332v3)
+        - [PARAMETRIC EXPONENTIAL LINEAR UNIT FOR DEEP CONVOLUTIONAL NEURAL NETWORKS](
+        https://arxiv.org/abs/1605.09332v3)
     """
 
     def __init__(self, alpha_initializer='ones',
@@ -93,7 +94,8 @@ class PELU(Layer):
             pos = K.relu(x) * (K.pattern_broadcast(self.alpha, self.param_broadcast) /
                                K.pattern_broadcast(self.beta, self.param_broadcast))
             neg = (K.pattern_broadcast(self.alpha, self.param_broadcast) *
-                   (K.exp((-K.relu(-x)) / K.pattern_broadcast(self.beta, self.param_broadcast)) - 1))
+                   (K.exp((-K.relu(-x))
+                          / K.pattern_broadcast(self.beta, self.param_broadcast)) - 1))
         else:
             pos = K.relu(x) * self.alpha / self.beta
             neg = self.alpha * (K.exp((-K.relu(-x)) / self.beta) - 1)
@@ -146,7 +148,8 @@ class SReLU(Layer):
             set `shared_axes=[1, 2]`.
 
     # References
-        - [Deep Learning with S-shaped Rectified Linear Activation Units](http://arxiv.org/abs/1512.07030)
+        - [Deep Learning with S-shaped Rectified Linear Activation Units](
+        http://arxiv.org/abs/1512.07030)
     """
 
     def __init__(self, t_left_initializer='zeros',
@@ -251,12 +254,14 @@ class Swish(Layer):
 
     # Arguments
         beta: float >= 0. Scaling factor
-            if set to 1 and trainable set to False (default), Swish equals the SiLU activation (Elfwing et al., 2017)
+            if set to 1 and trainable set to False (default),
+            Swish equals the SiLU activation (Elfwing et al., 2017)
         trainable: whether to learn the scaling factor during training or not
 
     # References
         - [Searching for Activation Functions](https://arxiv.org/abs/1710.05941)
-        - [Sigmoid-weighted linear units for neural network function approximation in reinforcement learning](https://arxiv.org/abs/1702.03118)
+        - [Sigmoid-weighted linear units for neural network function
+        approximation in reinforcement learning](https://arxiv.org/abs/1702.03118)
     """
 
     def __init__(self, beta=1.0, trainable=False, **kwargs):
@@ -290,8 +295,10 @@ class SineReLU(Layer):
 
     It allows an oscilation in the gradients when the weights are negative.
     The oscilation can be controlled with a parameter, which makes it be close
-    or equal to zero. The functional is diferentiable at any point due to its derivative.
-    For instance, at 0, the derivative of 'sin(0) - cos(0)' is 'cos(0) + sin(0)' which is 1.
+    or equal to zero. The functional is diferentiable at any point due to
+    its derivative.
+    For instance, at 0, the derivative of 'sin(0) - cos(0)'
+    is 'cos(0) + sin(0)' which is 1.
 
     # Input shape
         Arbitrary. Use the keyword argument `input_shape`
@@ -302,14 +309,18 @@ class SineReLU(Layer):
         Same shape as the input.
 
     # Arguments
-        epsilon: float. Hyper-parameter used to control the amplitude of the sinusoidal wave when weights are negative.
-                 The default value, 0.0025, since it works better for CNN layers and those are the most used layers nowadays.
-                 When using Dense Networks, try something around 0.006.
+        epsilon: float. Hyper-parameter used to control the amplitude of the
+            sinusoidal wave when weights are negative.
+            The default value, 0.0025, since it works better for CNN layers and
+            those are the most used layers nowadays.
+            When using Dense Networks, try something around 0.006.
 
     # References:
         - SineReLU: An Alternative to the ReLU Activation Function. This function was
-        first introduced at the Codemotion Amsterdam 2018 and then at the DevDays, in Vilnius, Lithuania.
-        It has been extensively tested with Deep Nets, CNNs, LSTMs, Residual Nets and GANs, based
+        first introduced at the Codemotion Amsterdam 2018 and then at
+        the DevDays, in Vilnius, Lithuania.
+        It has been extensively tested with Deep Nets, CNNs,
+        LSTMs, Residual Nets and GANs, based
         on the MNIST, Kaggle Toxicity and IMDB datasets.
 
     # Performance:
@@ -317,23 +328,35 @@ class SineReLU(Layer):
         - Fashion MNIST
           * Mean of 6 runs per Activation Function
             * Fully Connection Network
-              - SineReLU: loss mean -> 0.3522; accuracy mean -> 89.18; mean of std loss -> 0.08375204467435822
-              - LeakyReLU: loss mean-> 0.3553; accuracy mean -> 88.98; mean of std loss -> 0.0831161868455245
-              - ReLU: loss mean -> 0.3519; accuracy mean -> 88.84; mean of std loss -> 0.08358816501301362
+              - SineReLU: loss mean -> 0.3522; accuracy mean -> 89.18;
+                  mean of std loss -> 0.08375204467435822
+              - LeakyReLU: loss mean-> 0.3553; accuracy mean -> 88.98;
+              mean of std loss -> 0.0831161868455245
+              - ReLU: loss mean -> 0.3519; accuracy mean -> 88.84;
+              mean of std loss -> 0.08358816501301362
             * Convolutional Neural Network
-              - SineReLU: loss mean -> 0.2180; accuracy mean -> 92.49; mean of std loss -> 0.0781155784858847
-              - LeakyReLU: loss mean -> 0.2205; accuracy mean -> 92.37; mean of std loss -> 0.09273670474788205
-              - ReLU: loss mean -> 0.2144; accuracy mean -> 92.45; mean of std loss -> 0.09396114585977
+              - SineReLU: loss mean -> 0.2180; accuracy mean -> 92.49;
+              mean of std loss -> 0.0781155784858847
+              - LeakyReLU: loss mean -> 0.2205; accuracy mean -> 92.37;
+              mean of std loss -> 0.09273670474788205
+              - ReLU: loss mean -> 0.2144; accuracy mean -> 92.45;
+              mean of std loss -> 0.09396114585977
         - MNIST
           * Mean of 6 runs per Activation Function
             * Fully Connection Network
-              - SineReLU: loss mean -> 0.0623; accuracy mean -> 98.53; mean of std loss -> 0.06012015231824904
-              - LeakyReLU: loss mean-> 0.0623; accuracy mean -> 98.50; mean of std loss -> 0.06052147632835356
-              - ReLU: loss mean -> 0.0605; accuracy mean -> 98.49; mean of std loss -> 0.059599885665016096
+              - SineReLU: loss mean -> 0.0623; accuracy mean -> 98.53;
+              mean of std loss -> 0.06012015231824904
+              - LeakyReLU: loss mean-> 0.0623; accuracy mean -> 98.50;
+              mean of std loss -> 0.06052147632835356
+              - ReLU: loss mean -> 0.0605; accuracy mean -> 98.49;
+              mean of std loss -> 0.059599885665016096
             * Convolutional Neural Network
-              - SineReLU: loss mean -> 0.0198; accuracy mean -> 99.51; mean of std loss -> 0.0425338329550847
-              - LeakyReLU: loss mean -> 0.0216; accuracy mean -> 99.40; mean of std loss -> 0.04834468835196667
-              - ReLU: loss mean -> 0.0185; accuracy mean -> 99.49; mean of std loss -> 0.05503719489690131
+              - SineReLU: loss mean -> 0.0198; accuracy mean -> 99.51;
+              mean of std loss -> 0.0425338329550847
+              - LeakyReLU: loss mean -> 0.0216; accuracy mean -> 99.40;
+              mean of std loss -> 0.04834468835196667
+              - ReLU: loss mean -> 0.0185; accuracy mean -> 99.49;
+              mean of std loss -> 0.05503719489690131
 
     # Jupyter Notebooks
         - https://github.com/ekholabs/DLinK/blob/master/notebooks/keras
