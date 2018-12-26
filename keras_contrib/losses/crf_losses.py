7c3adcee39cbe2181a2692cdff6b87fb954ee5bb
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2018-12-26 14:28:33 +0100

    Shorten lines in different files. (#365)
    * Shorten lines in different files.
    
    * Added the raise section
    
    * style commit

--- keras_contrib/losses/crf_losses.py
+++ keras_contrib/losses/crf_losses.py
@@ -4,9 +4,10 @@ from keras.objectives import sparse_categorical_crossentropy
 
 
 def crf_nll(y_true, y_pred):
-    """The negative log-likelihood used to train the linear chain Conditional Random Field (CRF).
+    """The negative log-likelihood for linear chain Conditional Random Field (CRF).
 
-    This loss function is only used when the `layers.CRF` layer is trained in the "join" mode.
+    This loss function is only used when the `layers.CRF` layer
+    is trained in the "join" mode.
 
     # Arguments
         y_true: tensor with true targets.
@@ -15,12 +16,16 @@ def crf_nll(y_true, y_pred):
     # Returns
         A scalar representing corresponding to the negative log-likelihood.
 
+    # Raises
+        TypeError: If CRF is not the last layer.
+
     If you open an issue or a pull request about CRF, please
     add 'cc @lzfelix' to notify Luiz Felix.
     """
 
     crf, idx = y_pred._keras_history[:2]
-    assert not crf._outbound_nodes, 'When learn_model="join", CRF must be the last layer.'
+    if crf._outbound_nodes:
+        raise TypeError('When learn_model="join", CRF must be the last layer.')
     if crf.sparse_target:
         y_true = K.one_hot(K.cast(y_true[:, :, 0], 'int32'), crf.units)
     X = crf._inbound_nodes[idx].input_tensors[0]
