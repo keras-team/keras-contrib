99a6a228fffb7c4c24bd1842b40b5662382b1e3e
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2018-12-26 15:52:18 +0100

    Removed exceptions from the pytest.ini (#367)
    * Removed exceptions from the pytest.ini
    
    * Fixed docstring style.

--- keras_contrib/losses/jaccard.py
+++ keras_contrib/losses/jaccard.py
@@ -2,15 +2,20 @@ from keras import backend as K
 
 
 def jaccard_distance(y_true, y_pred, smooth=100):
-    """Jaccard distance for semantic segmentation, also known as the intersection-over-union loss.
+    """Jaccard distance for semantic segmentation.
+
+    Also known as the intersection-over-union loss.
 
     This loss is useful when you have unbalanced numbers of pixels within an image
     because it gives all classes equal weight. However, it is not the defacto
     standard for image segmentation.
 
-    For example, assume you are trying to predict if each pixel is cat, dog, or background.
-    You have 80% background pixels, 10% dog, and 10% cat. If the model predicts 100% background
-    should it be be 80% right (as with categorical cross entropy) or 30% (with this loss)?
+    For example, assume you are trying to predict if
+    each pixel is cat, dog, or background.
+    You have 80% background pixels, 10% dog, and 10% cat.
+    If the model predicts 100% background
+    should it be be 80% right (as with categorical cross entropy)
+    or 30% (with this loss)?
 
     The loss has been modified to have a smooth gradient as it converges on zero.
     This has been shifted so it converges on 0 and is smoothed to avoid exploding
