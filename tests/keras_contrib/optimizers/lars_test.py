965a49495212b98a74da088c38ebadf9cdaa12b7
Alex <roya0045@users.noreply.github.com>
2018-12-27 08:27:19 -0500

    Update lars_test.py

--- tests/keras_contrib/optimizers/lars_test.py
+++ tests/keras_contrib/optimizers/lars_test.py
@@ -2,5 +2,5 @@ from __future__ import print_function
 from keras_contrib.tests import optimizers
 from keras_contrib.optimizers import lars
 
-optimizers._test_optimizer(lars(0.01))
-optimizers._test_optimizer(lars(0.01, use_nesterov = True))
+optimizers._test_optimizer(lars.LARS(0.01))
+optimizers._test_optimizer(lars.LARS(0.01, use_nesterov = True))
