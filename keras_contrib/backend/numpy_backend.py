ab45320b2649c321db7c94c09cf8a0454a7a1f9a
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2018-12-29 22:13:36 +0100

    Added the numpy backend.

---
+++ keras_contrib/backend/numpy_backend.py
@@ -0,0 +1,21 @@
+import numpy as np
+from keras import backend as K
+
+
+def extract_image_patches(X, ksizes, strides,
+                          padding='valid',
+                          data_format='channels_first'):
+    raise NotImplementedError
+
+
+def depth_to_space(input, scale, data_format=None):
+    raise NotImplementedError
+
+
+def moments(x, axes, shift=None, keep_dims=False):
+    ''' Calculates and returns the mean and variance of the input '''
+
+    mean_batch = np.mean(x, axis=axes, keepdims=keep_dims)
+    var_batch = np.var(x, axis=axes, keepdims=keep_dims)
+
+    return mean_batch, var_batch
