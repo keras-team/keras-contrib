97219b5910b44f1b38fd7f5a97c511a06a0fedba
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2018-12-30 12:47:17 +0100

    Trying fewer changes.

--- convert_to_tf_keras.py
+++ convert_to_tf_keras.py
@@ -7,18 +7,30 @@ def replace_imports(file_path):
     with open(file_path, 'r') as f:
         text = f.read()
 
-    text = text.replace('import keras', 'import tensorflow.keras')
-    text = text.replace('from keras import', 'from tensorflow.keras import')
+    text_updated = text.replace('import keras', 'import tensorflow.keras')
+    # we don't want to catch from keras_contrib.
+    text_updated = text_updated.replace('from keras.', 'from tensorflow.keras.')
+    text_updated = text_updated.replace('from keras ', 'from tensorflow.keras ')
+
+    # temporary workaround because layer_test is not in the public API
+    text_updated = text_updated.replace(
+        'from keras.utils.test_utils import layer_test',
+        'from tensorflow.python.keras.testing_utils import layer_test')
 
     with open(file_path, 'w+') as f:
-        f.write(text)
+        f.write(text_updated)
+
+    return text_updated != text
 
 
 def main():
     """Run this function to convert the codebase to tf.keras"""
+    nb_of_files_changed = 0
     for root, dirs, files in os.walk("."):
         for name in files:
-            replace_imports(os.path.join(root, name))
+            if replace_imports(os.path.join(root, name)):
+                nb_of_files_changed += 1
+    print('Chnaged imports in ' + str(nb_of_files_changed) + ' files.')
 
 
 if __name__ == '__main__':
