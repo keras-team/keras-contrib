f6473128177444eba3dcfe396423dd394de1cad9
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2019-01-10 14:25:42 +0100

    Cleaning imports and namespaces. (#397)

--- keras_contrib/layers/pooling.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
-from .. import backend as K
-from keras.layers import Layer
-from keras.layers import InputSpec
-from keras_contrib.utils.conv_utils import conv_output_length
