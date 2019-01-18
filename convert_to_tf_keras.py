bbe16414db67a18b32deb5a2bea6209c4aca3ae0
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2019-01-18 15:23:04 +0100

    Added the back and forth conversion for tf.keras.

--- convert_to_tf_keras.py
+++ convert_to_tf_keras.py
@@ -1,7 +1,13 @@
 import os
 
+list_conversions = [('import keras.', 'import tensorflow.keras.'),
+                    ('import keras ', 'from tensorflow import keras '),
+                    ('import keras\n', 'from tensorflow import keras\n'),
+                    ('from keras.', 'from tensorflow.keras.'),
+                    ('from keras ', 'from tensorflow.keras ')]
 
-def replace_imports(file_path):
+
+def replace_imports(file_path, revert):
     if not file_path.endswith('.py'):
         return False
     if os.path.abspath(file_path) == os.path.abspath(__file__):
@@ -9,14 +15,14 @@ def replace_imports(file_path):
     with open(file_path, 'r') as f:
         text = f.read()
 
-    # we don't want to catch 'from keras_contrib'.
-    text_updated = text.replace('import keras.', 'import tensorflow.keras.')
-    text_updated = text_updated.replace('import keras ',
-                                        'from tensorflow import keras ')
-    text_updated = text_updated.replace('import keras\n',
-                                        'from tensorflow import keras\n')
-    text_updated = text_updated.replace('from keras.', 'from tensorflow.keras.')
-    text_updated = text_updated.replace('from keras ', 'from tensorflow.keras ')
+    if revert:
+        list_imports_to_change = [x[::-1] for x in list_conversions]
+    else:
+        list_imports_to_change = list_conversions
+
+    text_updated = text
+    for old_str, new_str in list_imports_to_change:
+        text_updated = text_updated.replace(old_str, new_str)
 
     with open(file_path, 'w+') as f:
         f.write(text_updated)
@@ -24,17 +30,27 @@ def replace_imports(file_path):
     return text_updated != text
 
 
-def convert_to_tf_keras():
-    """Run this function to convert the codebase to tf.keras"""
+def convert_codebase(revert):
     nb_of_files_changed = 0
     keras_dir = os.path.dirname(os.path.abspath(__file__))
     for root, dirs, files in os.walk(keras_dir):
         for name in files:
-            if replace_imports(os.path.join(root, name)):
+            if replace_imports(os.path.join(root, name), revert):
                 nb_of_files_changed += 1
     print('Changed imports in ' + str(nb_of_files_changed) + ' files.')
     print('Those files were found in the directory ' + keras_dir)
 
 
+def convert_to_tf_keras():
+    """Convert the codebase to tf.keras"""
+    convert_codebase(False)
+
+
+def convert_to_keras_team_keras():
+    """Convert the codebase from tf.keras to keras-team/keras"""
+    convert_codebase(True)
+
+
 if __name__ == '__main__':
     convert_to_tf_keras()
+    # convert_to_keras_team_keras()
