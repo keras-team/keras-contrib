104eb4e14ca5cc6a33f2c371b604a817fe407111
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-02-03 16:13:06 +0100

    Fixing the usage of leaked module attributes.

--- keras_contrib/layers/crf.py
+++ keras_contrib/layers/crf.py
@@ -458,7 +458,7 @@ class CRF(Layer):
             if K.backend() == 'theano':
                 m = states[3][:, t:(t + 2)]
             else:
-                m = K.tf.slice(states[3], [0, t], [-1, 2])
+                m = K.slice(states[3], [0, t], [-1, 2])
             input_energy_t = input_energy_t * K.expand_dims(m[:, 0])
             # (1, F, F)*(B, 1, 1) -> (B, F, F)
             chain_energy = chain_energy * K.expand_dims(
@@ -573,10 +573,14 @@ class CRF(Layer):
         def gather_each_row(params, indices):
             n = K.shape(indices)[0]
             if K.backend() == 'theano':
-                return params[K.T.arange(n), indices]
+                from theano import tensor as T
+                return params[T.arange(n), indices]
+            elif K.backend() == 'tensorflow':
+                import tensorflow as tf
+                indices = K.transpose(K.stack([tf.range(n), indices]))
+                return tf.gather_nd(params, indices)
             else:
-                indices = K.transpose(K.stack([K.tf.range(n), indices]))
-                return K.tf.gather_nd(params, indices)
+                raise NotImplementedError
 
         def find_path(argmin_table, best_idx):
             next_best_idx = gather_each_row(argmin_table, best_idx[0][:, 0])
