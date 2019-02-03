836ced4ad7224fbdacab044899c78ff97fba205c
Alex <roya0045@users.noreply.github.com>
2019-02-03 15:13:55 -0500

    Update lars_test.py

--- tests/keras_contrib/optimizers/lars_test.py
+++ tests/keras_contrib/optimizers/lars_test.py
@@ -12,7 +12,7 @@ def test_nesterov_lars():
   optimizers._test_optimizer(lars.LARS(0.01, nesterov=True))
   
 def test_skip_list():
-   X = np.random.randn((15, 30))
+   X = np.random.randn(15, 30)
    y = np.random.randn(3,size=(15,1))
    model = Sequential()
    model.add(Dense(32, activation='relu', input_dim=30))
