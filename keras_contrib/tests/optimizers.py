e2d260d9824438bc15a042a94fdbf901b5e84b18
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-02-03 16:43:32 +0100

    Used class.name.

--- keras_contrib/tests/optimizers.py
+++ keras_contrib/tests/optimizers.py
@@ -37,6 +37,6 @@ def _test_optimizer(optimizer, target=0.75):
     history = model.fit(x_train, y_train, epochs=2, batch_size=16, verbose=0)
     assert history.history['acc'][-1] >= target
     config = optimizers.serialize(optimizer)
-    optim = optimizers.deserialize(config, custom_objects={optimizer.__name__: optimizer.__class__})
+    optim = optimizers.deserialize(config, custom_objects={optimizer.__class__.__name__: optimizer.__class__})
     new_config = optimizers.serialize(optim)
     assert config == new_config
