a637d8cc7643254a54146fcabde0001868479a51
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-04 14:27:40 +0530

    Update capsule.py

--- keras_contrib/layers/capsule.py
+++ keras_contrib/layers/capsule.py
@@ -185,12 +185,12 @@ class Capsule(Layer):
         for i in range(self.routings):
             c = K.softmax(b, 1)
             o = K.batch_dot(c, u_hat_vecs, [2, 2])
-            if len(K.int_shape(o)) == 4:
+            if K.backend() == 'theano' or len(K.int_shape(o)) == 4:
                 o = K.sum(o, axis=1)
             if i < self.routings - 1:
                 o = K.l2_normalize(o, -1)
                 b = K.batch_dot(o, u_hat_vecs, [2, 3])
-                if len(K.int_shape(b)) == 4:
+                if K.backend() == 'theano' or len(K.int_shape(b)) == 4:
                     b = K.sum(b, axis=1)
 
         return self.activation(o)
