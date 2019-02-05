1a0afb1e6dd08c4ffafe9d79f508ee9fa444c125
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-06 03:09:42 +0530

    Add Capsule Layer to layers (capsule.py) (#406)
    * Update convolutional.py
    
    * Update convolutional.py
    
    * Update convolutional.py
    
    * Update convolutional.py
    
    * Update convolutional.py
    
    * Update convolutional.py
    
    * Update convolutional.py
    
    * Update convolutional.py
    
    * Update convolutional.py
    
    * Create capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Create test_capsule
    
    * Rename test_capsule to test_capsule.py
    
    * Update __init__.py
    
    * Update test_capsule.py
    
    * Update test_capsule.py
    
    * Update test_capsule.py
    
    * Update test_capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update test_capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Make all changes
    
    * Update test_capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update test_capsule.py
    
    * Fix initializer default value
    
    * Update capsule.py
    
    * Update test_capsule.py
    
    * Update test_capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Fix indentation mistakes
    
    * Add extra blank line
    
    * Improve variable names
    
    * Update capsule.py
    
    * Update capsule.py
    
    * Remove custom objects line
    
    * Update capsule.py

--- keras_contrib/layers/__init__.py
+++ keras_contrib/layers/__init__.py
@@ -12,5 +12,7 @@ from .core import CosineDense
 
 from .crf import CRF
 
+from .capsule import Capsule
+
 from .normalization import InstanceNormalization
 from .normalization import GroupNormalization
