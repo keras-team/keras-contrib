03fe0aab6d60768048d359d1805b7252cdb1cc65
Alex <roya0045@users.noreply.github.com>
2019-02-06 07:44:03 -0500

    removing skiplist

--- keras_contrib/optimizers/lars.py
+++ keras_contrib/optimizers/lars.py
@@ -25,11 +25,6 @@ class LARS(Optimizer):
             highest scaling factor in LARS.
         epsilon: Optional epsilon parameter to be set in models that have very
             small gradients. Default set to 0.0.
-        skip_list: List of strings to enable skipping variables from LARS scaling.
-            If any of the strings in skip_list is a subset of var.name, variable
-            'var' is skipped from LARS scaling. For a typical classification model
-            with batch normalization, the skip_list is ['batch_normalization',
-            'bias']
         nesterov: when set to True, nesterov momentum will be enabled
     """
 
@@ -39,7 +34,6 @@ class LARS(Optimizer):
                  weight_decay=0.0001,
                  eeta=0.001,
                  epsilon=0.0,
-                 skip_list=None,
                  nesterov=False,
                  **kwargs):
 
@@ -55,7 +49,6 @@ class LARS(Optimizer):
             self.weight_decay = K.variable(weight_decay, name='weight_decay')
             self.eeta = K.variable(eeta, name='eeta')
         self.epsilon = epsilon
-        self.skip_list = skip_list
         self.nesterov = nesterov
 
 
@@ -64,17 +57,15 @@ class LARS(Optimizer):
         weights = self.get_weights()
         self.updates = [K.update_add(self.iterations, 1)]
         scaled_lr = self.lr
-        if self.skip_list is None or not any(p in params.name
-                                             for p in self.skip_list):
-            w_norm = K.sqrt(K.sum([K.sum(K.square(weight))
-                                   for weight in weights]))
-            g_norm = K.sqrt(K.sum([K.sum(K.square(grad))
-                                   for grad in grads]))
-            scaled_lr = K.switch(K.greater(w_norm * g_norm, K.zeros([1])),
-                                 K.expand_dims((self.eeta * w_norm /
-                                                (g_norm + self.weight_decay * w_norm +
-                                                 self.epsilon)) * self.lr),
-                                 K.ones([1]) * self.lr)
+        w_norm = K.sqrt(K.sum([K.sum(K.square(weight))
+                               for weight in weights]))
+        g_norm = K.sqrt(K.sum([K.sum(K.square(grad))
+                               for grad in grads]))
+        scaled_lr = K.switch(K.greater(w_norm * g_norm, K.zeros([1])),
+                             K.expand_dims((self.eeta * w_norm /
+                                            (g_norm + self.weight_decay * w_norm +
+                                             self.epsilon)) * self.lr),
+                             K.ones([1]) * self.lr)
         if K.backend() == 'theano':
             scaled_lr = scaled_lr[0]  # otherwise theano raise broadcasting error
         # momentum
@@ -105,7 +96,6 @@ class LARS(Optimizer):
                   'weight_decay': float(K.get_value(self.weight_decay)),
                   'epsilon': self.epsilon,
                   'eeta': float(K.get_value(self.eeta)),
-                  'nesterov': self.nesterov,
-                  'skip_list': self.skip_list}
+                  'nesterov': self.nesterov}
         base_config = super(LARS, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
