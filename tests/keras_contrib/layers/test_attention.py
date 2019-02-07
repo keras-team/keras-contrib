0498562f1b7496c51267499b96035f448edc9126
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-07 20:57:17 +0530

    Add missing import in test

--- tests/keras_contrib/layers/test_attention.py
+++ tests/keras_contrib/layers/test_attention.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 
 from keras_contrib.utils.test_utils import layer_test
+from keras_contrib.layers.attention import Attention
 from keras import backend as K
 from keras.models import Sequential
 
