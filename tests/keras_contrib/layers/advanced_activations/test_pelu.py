78d21c0b5bd55b993b947edf920300a010c5ca7b
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-02-10 15:15:38 +0100

    Forgot to change the import.

--- tests/keras_contrib/layers/advanced_activations/test_pelu.py
+++ tests/keras_contrib/layers/advanced_activations/test_pelu.py
@@ -1,11 +1,11 @@
 import pytest
 from keras_contrib.utils.test_utils import layer_test
-from keras_contrib.layers import advanced_activations
+from keras_contrib.layers import PELU
 
 
 @pytest.mark.parametrize('kwargs', [{}, {'shared_axes': 1}])
 def test_pelu(kwargs):
-    layer_test(advanced_activations.PELU, kwargs=kwargs,
+    layer_test(PELU, kwargs=kwargs,
                input_shape=(2, 3, 4))
 
 
