8497f72e6cb9a2f80947fea733859025db74968e
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-10 23:19:00 +0530

    Fix import error

--- tests/keras_contrib/layers/advanced_activations/test_pseu.py
+++ tests/keras_contrib/layers/advanced_activations/test_pseu.py
@@ -1,13 +1,13 @@
 import pytest
 from keras_contrib.utils.test_utils import layer_test
-from keras_contrib.layers import advanced_activations
+from keras_contrib.layers import PSEU
 
 
 @pytest.mark.parametrize('trainable', [True, False])
 @pytest.mark.parametrize('alpha_init', [-0.1, 0, 0.1])
 def test_pseu(trainable,
               alpha_init):
-    layer_test(advanced_activations.PSEU,
+    layer_test(PSEU,
                kwargs={'alpha_init': alpha_init,
                        'trainable': trainable},
                input_shape=(2, 3, 4))
