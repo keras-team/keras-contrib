d6cf865db69da7b9fab9de4c5e5fd3b63f0f2af0
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-10 21:39:10 +0530

    Add PSEU test

--- tests/keras_contrib/layers/advanced_activations/test_pseu.py
+++ tests/keras_contrib/layers/advanced_activations/test_pseu.py
@@ -1,1 +1,10 @@
+import pytest
+from keras_contrib.utils.test_utils import layer_test
+from keras_contrib.layers import advanced_activations
 
+
+@pytest.mark.parametrize('kwargs', [{}, {'shared_axes': 1}])
+def test_pseu(kwargs):
+    layer_test(advanced_activations.PSEU,
+               kwargs={'alpha_init': 0.1},
+               input_shape=(2, 3, 4))
