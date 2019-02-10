c9ac0e4abc86b9698f3a9a490977a7f205d4f8d8
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-10 22:29:14 +0530

    Update PSEU layer

--- keras_contrib/layers/advanced_activations/pseu.py
+++ keras_contrib/layers/advanced_activations/pseu.py
@@ -1,3 +1,10 @@
+from keras import initializers
+from keras import regularizers
+from keras import constraints
+from keras.layers import Layer
+from keras import backend as K
+
+
 class PSEU(Layer):
     """Parametric Soft Exponential Unit with trainable alpha
     See: https://arxiv.org/pdf/1602.01321.pdf by Godfrey and Gashler
@@ -9,52 +16,82 @@ class PSEU(Layer):
     # Output shape
         Same shape as the input.
     # Arguments
-        alpha_init: float. Initial value of the alpha weights.
-        weights: initial alpha weights, as a list of 1 numpy array.
-                 if both weights & alpha_init are provided, weights
-                 overrides alpha_init
+        initializer: Initializer for alpha weights.
+        alpha_init: Initial value of the alpha weights (float)
+                    This value overrides any specified initializer
+                    by default, but, one can use their initializer
+                    of choice by specifying alpha_init=None.
+        regularizer: Regularizer for alpha weights.
+        constraint: Constraint for alpha weights.
+        trainable: Whether the alpha weights are trainable or not
+
     # Example
         model = Sequential()
         model.add(Dense(10))
         model.add(PSEU())
+
+    Note : Specify alpha_init=None to use other intializers
+
     Soft Exponential f(α, x):
         α == 0:  x
         α  > 0:  (exp(αx)-1) / α + α
         α  < 0:  -ln(1-α(x + α)) / α
     """
-    def __init__(self, alpha_init=0.1,
-                 weights=None, **kwargs):
+    def __init__(self,
+                 alpha_init=0.1,
+                 initializer='glorot_uniform',
+                 regularizer=None,
+                 constraint=None,
+                 trainable=True,
+                 **kwargs):
+
         self.supports_masking = True
-        self.alpha_init = K.cast_to_floatx(alpha_init)
-        self.initial_weights = weights
+        self.alpha_init = alpha_init
+        self.initializer = initializers.get(initializer)
+        self.regularizer = regularizers.get(regularizer)
+        self.constraint = constraints.get(constraint)
+        self.trainable = trainable
+
         super(PSEU, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        input_shape = input_shape[1:]
-        self.alphas = K.variable(self.alpha_init * np.ones(input_shape),
-                                 name='{}_alphas'.format(self.name))
-        self.trainable_weights = [self.alphas]
+        new_input_shape = input_shape[1:]
+        self.alphas = self.add_weight(shape=new_input_shape,
+                                      name='{}_alphas'.format(self.name),
+                                      initializer=self.initializer,
+                                      regularizer=self.regularizer,
+                                      constraint=self.constraint)
 
-        if self.initial_weights is not None:
-            self.set_weights(self.initial_weights)
-            del self.initial_weights
+        if self.trainable:
+            self.trainable_weights = [self.alphas]
+
+        if self.alpha_init is not None:
+            self.set_weights([self.alpha_init*np.ones(new_input_shape)])
 
         self.build = True
 
-    def call_alpha_gt0(self, x, alpha):
-        return alpha + (K.exp(alpha * x) - 1.) / alpha
-
-    def call_alpha_lt0(self, x, alpha):
-        return - K.log(1 - alpha * (x + alpha)) / alpha
-
     def call(self, x, mask=None):
-        return K.switch(self.alphas > 0, self.call_alpha_gt0,
-                        K.switch(self.alphas < 0, self.call_alpha_lt0, x))
+        if K.eval(self.alphas) < 0:
+            return - K.log(1 - self.alphas * (x + self.alphas)) / self.alphas
+        elif K.eval(self.alphas) > 0:
+            return self.alphas + (K.exp(self.alphas * x) - 1.) / self.alphas
+        else:
+            return x
 
     def compute_output_shape(self, input_shape):
         return input_shape
 
     def get_config(self):
-        config = {'alpha_init': float(self.alpha_init)}
-        base_config = super(ParametricSoftExp, self).get_config()
+        if self.alpha_init is None:
+            config = {'alpha_initializer': initializers.serialize(self.initializer),
+                      'regularizer': regularizers.serialize(self.regularizer),
+                      'constraint': constraints.serialize(self.constraint),
+                      'trainable': self.trainable}
+        else:
+            config = {'alpha_initial': float(self.alpha_init),
+                      'regularizer': regularizers.serialize(self.regularizer),
+                      'constraint': constraints.serialize(self.constraint),
+                      'trainable': self.trainable}
+
+        base_config = super(PSEU, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
