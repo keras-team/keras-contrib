7f64eaa62260437a18c87b90b4db2b375f392dc2
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-02-10 19:15:26 +0100

    Updated the exclude list.

--- tests/tooling/test_codeowners.py
+++ tests/tooling/test_codeowners.py
@@ -66,11 +66,6 @@ directories_to_test = [
 ]
 directories_to_test = [path_to_keras_contrib / x for x in directories_to_test]
 
-pattern_exclude = [
-    '.gitkeep',
-    '__init__.py'
-]
-
 # TODO: remove those files or find them owners.
 exclude = [
     'examples/cifar10_clr.py',
@@ -82,14 +77,9 @@ exclude = [
     'examples/conll2000_chunking_crf.py',
     'examples/improved_wgan.py',
     'examples/jaccard_loss.py',
-    'keras_contrib/activations/squash.py',
     'keras_contrib/callbacks/cyclical_learning_rate.py',
     'keras_contrib/callbacks/dead_relu_detector.py',
-    'keras_contrib/callbacks/snapshot.py',
-    'keras_contrib/applications/densenet.py',
-    'keras_contrib/applications/nasnet.py',
     'keras_contrib/applications/resnet.py',
-    'keras_contrib/applications/wide_resnet.py',
     'keras_contrib/constraints/clip.py',
     'keras_contrib/datasets/coco.py',
     'keras_contrib/datasets/conll2000.py',
@@ -98,16 +88,14 @@ exclude = [
     'keras_contrib/losses/crf_losses.py',
     'keras_contrib/losses/dssim.py',
     'keras_contrib/losses/jaccard.py',
-    'keras_contrib/layers/advanced_activations.py',
-    'keras_contrib/layers/capsule.py',
-    'keras_contrib/layers/convolutional.py',
+    'keras_contrib/layers/advanced_activations/pelu.py',
+    'keras_contrib/layers/advanced_activations/srelu.py',
+    'keras_contrib/layers/convolutional/cosineconvolution2d.py',
     'keras_contrib/layers/core.py',
     'keras_contrib/layers/crf.py',
-    'keras_contrib/layers/normalization.py',
+    'keras_contrib/layers/normalization/instancenormalization.py',
     'keras_contrib/optimizers/ftml.py',
     'keras_contrib/optimizers/lars.py',
-    'keras_contrib/optimizers/padam.py',
-    'keras_contrib/optimizers/yogi.py',
     'keras_contrib/metrics/crf_accuracies.py',
 ]
 exclude = [path_to_keras_contrib / x for x in exclude]
@@ -116,11 +104,16 @@ exclude = [path_to_keras_contrib / x for x in exclude]
 @pytest.mark.parametrize('directory', directories_to_test)
 def test_all_files_have_owners(directory):
     files_with_owners = [x[0] for x in parse_codeowners()]
-    for child in directory.iterdir():
-        if child.is_file():
-            if child.name in pattern_exclude or child in exclude:
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            file_path = pathlib.Path(root) / name
+            if file_path.suffix != '.py':
                 continue
-            assert child in files_with_owners
+            if file_path.name == '__init__.py':
+                continue
+            if file_path in exclude:
+                continue
+            assert file_path in files_with_owners
 
 
 if __name__ == '__main__':
