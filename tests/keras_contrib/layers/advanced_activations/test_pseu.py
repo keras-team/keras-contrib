542db44a73e5b3f3168f9ad6aafa9a0522b3b905
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-10 22:56:37 +0530

    Fix silly errors in test

--- tests/keras_contrib/layers/advanced_activations/test_pseu.py
+++ tests/keras_contrib/layers/advanced_activations/test_pseu.py
@@ -5,8 +5,12 @@ from keras_contrib.layers import advanced_activations
 
 @pytest.mark.parametrize('trainable', [True, False])
 @pytest.mark.parametrize('alpha_init', [-0.1, 0, 0.1])
-def test_pseu(kwargs):
+def test_pseu(trainable,
+              alpha_init):
     layer_test(advanced_activations.PSEU,
                kwargs={'alpha_init': alpha_init,
                        'trainable': trainable},
                input_shape=(2, 3, 4))
+
+if __name__ == '__main__':
+    pytest.main([__file__])
