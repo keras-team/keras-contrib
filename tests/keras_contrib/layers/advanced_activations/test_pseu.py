abd58ae61b51e529d21e07ae4db1d689bcc5a700
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-10 22:32:36 +0530

    Update test

--- tests/keras_contrib/layers/advanced_activations/test_pseu.py
+++ tests/keras_contrib/layers/advanced_activations/test_pseu.py
@@ -3,8 +3,10 @@ from keras_contrib.utils.test_utils import layer_test
 from keras_contrib.layers import advanced_activations
 
 
-@pytest.mark.parametrize('kwargs', [{}, {'shared_axes': 1}])
+@pytest.mark.parametrize('trainable', [True, False])
+@pytest.mark.parametrize('alpha_init', [-0.1, 0, 0.1])
 def test_pseu(kwargs):
     layer_test(advanced_activations.PSEU,
-               kwargs={'alpha_init': 0.1},
+               kwargs={'alpha_init': alpha_init,
+                       'trainable': trainable},
                input_shape=(2, 3, 4))
