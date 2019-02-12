8791343eb738d7c8051a4728edcfefa1aeaa9b06
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-12 18:31:32 +0530

    Remove trainable param from test

--- tests/keras_contrib/layers/advanced_activations/test_pseu.py
+++ tests/keras_contrib/layers/advanced_activations/test_pseu.py
@@ -4,13 +4,10 @@ from keras_contrib.utils.test_utils import layer_test
 from keras_contrib.layers import PSEU
 
 
-@pytest.mark.parametrize('trainable', [True, False])
 @pytest.mark.parametrize('alpha_init', [-0.1, 0., 0.1])
-def test_pseu(alpha_init,
-              trainable):
+def test_pseu(alpha_init):
     layer_test(PSEU,
-               kwargs={'trainable': trainable,
-                       'alpha_init': alpha_init},
+               kwargs={'alpha_init': alpha_init},
                input_shape=(2, 3, 4))
 
 
