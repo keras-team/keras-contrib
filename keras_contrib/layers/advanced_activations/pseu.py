ef4a20fe64c394099936e54c64786e9f71a5a8dd
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-12 20:49:08 +0530

    Simplify code

--- keras_contrib/layers/advanced_activations/pseu.py
+++ keras_contrib/layers/advanced_activations/pseu.py
@@ -7,6 +7,12 @@ class PSEU(Layer):
     """Parametric Soft Exponential Unit
     See: https://arxiv.org/pdf/1602.01321.pdf by Godfrey and Gashler
     Reference: https://github.com/keras-team/keras/issues/3842 (@hobson)
+
+    Soft Exponential f(α, x):
+        α == 0:  x
+        α  > 0:  (exp(αx)-1) / α + α
+        α  < 0:  -ln(1-α(x + α)) / α
+
     # Input shape
         Arbitrary. Use the keyword argument `input_shape`
         (tuple of integers, does not include the samples axis)
@@ -16,27 +22,22 @@ class PSEU(Layer):
         Same shape as the input.
 
     # Arguments
-        alpha_initial: Initial value of the alpha weights (float)
+        alpha: Value of the alpha weights (float)
 
     # Example
         model = Sequential()
-        model.add(Dense(10))
-        model.add(PSEU(alpha_initial=0.2))
-
-    Soft Exponential f(α, x):
-        α == 0:  x
-        α  > 0:  (exp(αx)-1) / α + α
-        α  < 0:  -ln(1-α(x + α)) / α
+        model.add(Dense(10, input_shape=(5,))
+        model.add(PSEU(alpha=0.2))
     """
     def __init__(self,
-                 alpha_initial=0.1,
+                 alpha=0.1,
                  **kwargs):
 
         super(PSEU, self).__init__(**kwargs)
-        self.alpha_initial = alpha_initial
+        self.alpha = alpha
 
     def alpha_initializer(self, input_shape):
-        return self.alpha_initial * K.ones(input_shape)
+        return self.alpha * K.ones(input_shape)
 
     def build(self, input_shape):
         new_input_shape = input_shape[1:]
@@ -46,9 +47,9 @@ class PSEU(Layer):
         self.build = True
 
     def call(self, x):
-        if self.alpha_initial < 0:
+        if self.alpha < 0:
             return - K.log(1 - self.alphas * (x + self.alphas)) / self.alphas
-        elif self.alpha_initial > 0:
+        elif self.alpha > 0:
             return self.alphas + (K.exp(self.alphas * x) - 1.) / self.alphas
         else:
             return x
@@ -57,6 +58,6 @@ class PSEU(Layer):
         return input_shape
 
     def get_config(self):
-        config = {'alpha_initial': self.alpha_initial}
+        config = {'alpha': self.alpha}
         base_config = super(PSEU, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
