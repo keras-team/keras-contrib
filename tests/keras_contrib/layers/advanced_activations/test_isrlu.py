ece43b32f8d75d390d6b1cb8dd989c7735c75eec
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-13 23:36:16 +0530

    Add test

--- tests/keras_contrib/layers/advanced_activations/test_isrlu.py
+++ tests/keras_contrib/layers/advanced_activations/test_isrlu.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
+import pytest
+from keras_contrib.utils.test_utils import layer_test
+from keras_contrib.layers import ISRLU
 
+
+@pytest.mark.parametrize('alpha', [0.2, 0.3, -0.01])
+def test_isrlu(alpha):
+    layer_test(ISRLU,
+               kwargs={'alpha': alpha},
+               input_shape=(2, 3, 4))
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
