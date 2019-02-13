f035930d9912ecf73b909d8e295b88842cb7889f
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2019-02-13 23:24:37 +0530

    Fix test

--- tests/keras_contrib/layers/advanced_activations/test_isrlu.py
+++ tests/keras_contrib/layers/advanced_activations/test_isrlu.py
@@ -4,7 +4,7 @@ from keras_contrib.utils.test_utils import layer_test
 from keras_contrib.layers import ISRLU
 
 
-@pytest.mark.parametrize('alpha', [0.2, 0.3, 0.01])
+@pytest.mark.parametrize('alpha', [0.2, 0.3, -0.01])
 def test_isrlu(alpha):
     layer_test(ISRLU,
                kwargs={'alpha': alpha},
